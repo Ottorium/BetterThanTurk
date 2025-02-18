a41fc309ebe1e653d0f88c7b80e1d43e6098f1e4
Michael Ludwig <michael.ludwig1392@gmail.com>
2025-02-18 13:58:21 +0100

    project setup

---
+++ chess/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module at.htlhl.chess {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens at.htlhl.chess to javafx.fxml;
+    exports at.htlhl.chess;
+}
