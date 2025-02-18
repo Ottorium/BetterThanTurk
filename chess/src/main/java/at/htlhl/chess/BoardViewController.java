ed719877bfe0d4b4997ee232f48c441518cfe714
Michael Ludwig <michael.ludwig1392@gmail.com>
2025-02-18 16:12:00 +0100

    make basic board

---
+++ chess/src/main/java/at/htlhl/chess/BoardViewController.java
@@ -0,0 +1,42 @@
+package at.htlhl.chess;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BoardViewController implements Initializable {
+
+    @FXML
+    private GridPane chessBoard;
+
+    private static final int BOARD_SIZE = 8;
+    private static final int INITIAL_SQUARE_SIZE = 60;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        createEmptyChessBoard();
+    }
+
+    private void createEmptyChessBoard() {
+        for (int row = 0; row < BOARD_SIZE; row++) {
+            for (int col = 0; col < BOARD_SIZE; col++) {
+                StackPane squarePane = new StackPane();
+                Rectangle square = new Rectangle(INITIAL_SQUARE_SIZE, INITIAL_SQUARE_SIZE);
+
+                if ((row + col) % 2 == 0)
+                    square.setFill(Color.rgb(242, 226, 190)); // Light beige/cream for light squares
+                else
+                    square.setFill(Color.rgb(176, 136, 104)); // Medium brown for dark squares
+
+                squarePane.getChildren().add(square);
+                chessBoard.add(squarePane, col, row);
+            }
+        }
+    }
+}
--- chess/src/main/java/at/htlhl/chess/ChessApplication.java
+++ chess/src/main/java/at/htlhl/chess/ChessApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class ChessApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(ChessApplication.class.getResource("board-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 8 * 60, 8 * 60);
+        stage.setTitle("Chess");
         stage.setScene(scene);
         stage.show();
     }
