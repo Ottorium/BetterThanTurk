c86929435bf250ae714ab740bf6cb17cb78cfc6f
Michael Ludwig <michael.ludwig1392@gmail.com>
2025-03-13 18:44:11 +0100

    implement basic engine

--- chess/src/main/java/at/htlhl/chess/boardlogic/engine/Engine.java
+++ chess/src/main/java/at/htlhl/chess/boardlogic/engine/Engine.java
@@ -16,13 +16,59 @@ public class Engine {
         this.field = field.clone();
     }
 
+    public void setField(Field field) {
+        this.field = field.clone();
+    }
+
     public Move getBestMove(String fen) {
         field.trySetFEN(fen);
         return getBestMove();
     }
 
     public Move getBestMove() {
-        var moves = field.getMoveChecker().getAllLegalMoves();
-        return moves.isEmpty() ? null : moves.getFirst();
+        if (field.isBlackTurn()) return null;
+        Move bestMove = null;
+        var bestScore = Integer.MIN_VALUE;
+        for (Move move : field.getMoveChecker().getAllLegalMoves()) {
+            var score = maxi(3);
+            if (score > bestScore) {
+                bestScore = score;
+                bestMove = move;
+            }
+        }
+        System.out.println(bestScore);
+        System.out.println(bestMove);
+        return bestMove;
     }
- }
+
+    int maxi(int depth) {
+        if (depth == 0) return evaluateCurrentPosition();
+        int max = Integer.MIN_VALUE;
+        for (var move : field.getMoveChecker().getAllLegalMoves()) {
+            Field before = field.clone();
+            field.forceMove(move, false);
+            var score = mini(depth - 1);
+            max = Math.max(score, max);
+            field = before;
+        }
+        return max;
+    }
+
+    int mini(int depth) {
+        if (depth == 0) return -evaluateCurrentPosition();
+        int min = Integer.MAX_VALUE;
+        for (var move : field.getMoveChecker().getAllLegalMoves()) {
+            Field before = field.clone();
+            field.forceMove(move, false);
+            var score = maxi(depth - 1);
+            min = Math.min(min, score);
+            field = before;
+        }
+        return min;
+    }
+
+
+    private int evaluateCurrentPosition() {
+        return field.getPieceEvaluation();
+    }
+}
