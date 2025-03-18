6cb2de63bda55b25522ee1b5ad5a07b8e538c4c1
Michael Ludwig <michael.ludwig1392@gmail.com>
2025-03-18 14:19:31 +0100

    make tests

--- chess/src/test/java/at/htlhl/chess/boardlogic/util/FENParserTest.java
+++ chess/src/test/java/at/htlhl/chess/boardlogic/util/FENParserTest.java
@@ -2,6 +2,7 @@ package at.htlhl.chess.boardlogic.util;
 
 import at.htlhl.chess.boardlogic.Square;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FENParserTest {
@@ -81,5 +82,102 @@ class FENParserTest {
         assertEquals(3, new FENParser(EARLY_GAME_FEN).parseNumberOfNextMove());
         assertEquals(12, new FENParser(COMPLEX_CASTLING_FEN).parseNumberOfNextMove());
         assertEquals(60, new FENParser(ENDGAME_FEN).parseNumberOfNextMove());
+    }
+
+    private byte[][] createBoardFromFEN(String fen) {
+        return new FENParser(fen).parseBoard();
+    }
+
+    @Test
+    void exportToFEN_InitialPosition_ShouldReturnCorrectFEN() {
+        byte[][] board = createBoardFromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+        String result = FENParser.exportToFEN(
+                board,
+                false, // white's turn
+                CastlingUtil.add(CastlingUtil.add(CastlingUtil.add(CastlingUtil.add(
+                                        CastlingUtil.INITIAL_NO_RIGHTS, CastlingUtil.WHITE_KING_SIDE),
+                                CastlingUtil.WHITE_QUEEN_SIDE), CastlingUtil.BLACK_KING_SIDE),
+                        CastlingUtil.BLACK_QUEEN_SIDE),
+                null, // no en passant
+                0,    // half moves
+                1     // next move
+        );
+        assertEquals("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", result);
+    }
+
+    @Test
+    void exportToFEN_EarlyGamePosition_ShouldReturnCorrectFEN() {
+        byte[][] board = createBoardFromFEN("r1bqkbnr/pppp1ppp/2n5/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R w KQkq - 2 3");
+        String result = FENParser.exportToFEN(
+                board,
+                false, // white's turn
+                CastlingUtil.add(CastlingUtil.add(CastlingUtil.add(CastlingUtil.add(
+                                        CastlingUtil.INITIAL_NO_RIGHTS, CastlingUtil.WHITE_KING_SIDE),
+                                CastlingUtil.WHITE_QUEEN_SIDE), CastlingUtil.BLACK_KING_SIDE),
+                        CastlingUtil.BLACK_QUEEN_SIDE),
+                null, // no en passant
+                2,    // half moves
+                3     // next move
+        );
+        assertEquals("r1bqkbnr/pppp1ppp/2n5/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R w KQkq - 2 3", result);
+    }
+
+    @Test
+    void exportToFEN_EnPassantPosition_ShouldReturnCorrectFEN() {
+        byte[][] board = createBoardFromFEN("rnbqkb1r/pppp1ppp/5n2/4p3/3P4/2N5/PPP1PPPP/R1BQKBNR w KQkq e6 0 3");
+        String result = FENParser.exportToFEN(
+                board,
+                false, // white's turn
+                CastlingUtil.add(CastlingUtil.add(CastlingUtil.add(CastlingUtil.add(
+                                        CastlingUtil.INITIAL_NO_RIGHTS, CastlingUtil.WHITE_KING_SIDE),
+                                CastlingUtil.WHITE_QUEEN_SIDE), CastlingUtil.BLACK_KING_SIDE),
+                        CastlingUtil.BLACK_QUEEN_SIDE),
+                Square.parseString("e6"), // en passant square
+                0,    // half moves
+                3     // next move
+        );
+        assertEquals("rnbqkb1r/pppp1ppp/5n2/4p3/3P4/2N5/PPP1PPPP/R1BQKBNR w KQkq e6 0 3", result);
+    }
+
+    @Test
+    void exportToFEN_PartialCastlingPosition_ShouldReturnCorrectFEN() {
+        byte[][] board = createBoardFromFEN("rnbq1bnr/ppppkppp/8/8/8/8/PPPP1PPP/RNBQ1RK1 b - - 2 6");
+        String result = FENParser.exportToFEN(
+                board,
+                true, // black's turn
+                CastlingUtil.INITIAL_NO_RIGHTS, // no castling rights
+                null, // no en passant
+                2,    // half moves
+                6     // next move
+        );
+        assertEquals("rnbq1bnr/ppppkppp/8/8/8/8/PPPP1PPP/RNBQ1RK1 b - - 2 6", result);
+    }
+
+    @Test
+    void exportToFEN_ComplexCastlingPosition_ShouldReturnCorrectFEN() {
+        byte[][] board = createBoardFromFEN("r3k2r/ppp2ppp/2n1b3/3np3/2B5/2N1P3/PPP2PPP/2KR3R b q - 14 12");
+        String result = FENParser.exportToFEN(
+                board,
+                true, // black's turn
+                CastlingUtil.add(CastlingUtil.INITIAL_NO_RIGHTS, CastlingUtil.BLACK_QUEEN_SIDE), // only black queen-side castling
+                null, // no en passant
+                14,   // half moves
+                12    // next move
+        );
+        assertEquals("r3k2r/ppp2ppp/2n1b3/3np3/2B5/2N1P3/PPP2PPP/2KR3R b q - 14 12", result);
+    }
+
+    @Test
+    void exportToFEN_EndgamePosition_ShouldReturnCorrectFEN() {
+        byte[][] board = createBoardFromFEN("4k3/8/8/8/8/3K4/8/8 b - - 50 60");
+        String result = FENParser.exportToFEN(
+                board,
+                true, // black's turn
+                CastlingUtil.INITIAL_NO_RIGHTS, // no castling rights
+                null, // no en passant
+                50,   // half moves
+                60    // next move
+        );
+        assertEquals("4k3/8/8/8/8/3K4/8/8 b - - 50 60", result);
     }
 }
